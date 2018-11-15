99e10186e4349bf39c4217951f91424569874157
Alexandr Golov <alexgolov.job@yandex.ru>
2018-11-15 14:38:53 +0300

    Созданы страницы логина и регистрации

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+import NewAd from '@/components/Ads/NewAd'
+import AdList from '@/components/Ads/AdList'
+import Ad from '@/components/Ads/Ad'
+import Orders from '@/components/User/Orders'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'reg',
+      component: Registration
+    },
+    {
+      path: '/new',
+      name: 'newAd',
+      component: NewAd
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: AdList
+    },
+    {
+      path: '/ad',
+      name: 'ad',
+      component: Ad
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders
+    }
+  ],
+  mode: 'history'
+})
